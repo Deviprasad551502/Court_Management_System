cb1376e0a11750b45004b28b16a1435202c078fd
deviprasad <interndeviprasadn@unicourt.com>
2026-01-28 19:01:44 +0530

    rough 2

---
+++ backend/src/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  schema: process.env.DB_SCHEMA,
+  autoLoadEntities: true,
+  synchronize: false,
+  migrationsRun: false,
+  migrations: ['dist/migrations/*.js'],
+};
