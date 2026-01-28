fadafbb02c551e661522e0f8fe6a9ddcf8a3469a
deviprasad <interndeviprasadn@unicourt.com>
2026-01-28 18:30:54 +0530

    rough

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dataSourceOptions} from './config/typeorm.config';
-import { CourtTypesModule } from './court-types/court-types.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot(dataSourceOptions),
-    CourtTypesModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/court-types/dto/create-court-type.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, Length } from 'class-validator';
-
-export class CreateCourtTypeDto {
-  @IsString()
-  @Length(3, 100)
-  name: string;
-}
